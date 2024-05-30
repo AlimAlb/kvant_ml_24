035fbf44a1f31a26ff02f9bfeaebe17f77ef7dde
Alim Albogachiev <alimalbogachiev@yandex.ru>
2024-05-30 16:15:28 +0300

    some of the tasks

---
+++ input.py
@@ -0,0 +1,5 @@
+name = input() # считываем имя
+ 
+s = "My name is " + name # склеиваем имя со строкой
+
+print(s)
---
+++ integers_and_arithmetics.py
@@ -0,0 +1,25 @@
+a = 7
+b = 4
+
+res = a + b # сумма
+print(res)
+
+res = a - b #разность
+print(res)
+
+res = a * b # умножение
+print(res)
+
+res = a / b # деление
+print(res)
+
+res = a // b # целочисленное деление
+print(res)
+
+res = a % b # остаток от деления
+print(res)
+
+res = a ** b # возведение в степень
+print(res)
+
+
